872aab6fc041e4223306bd99b86697a63b18234f
Patrick Malcolm <patrick.malcolm@tum.de>
2020-05-02 20:06:50 +0200

    add support for bus stops

--- SumoNetVis/Additionals.py
+++ SumoNetVis/Additionals.py
@@ -6,9 +6,36 @@ import warnings
 import xml.etree.ElementTree as ET
 from shapely.geometry import *
 from shapely import ops
+import numpy as np
 import matplotlib.patches
 import matplotlib.pyplot as plt
 from SumoNetVis import _Utils
+from SumoNetVis import Net
+
+
+BUS_STOP_STYLE_SUMO = "SUMO"
+BUS_STOP_STYLE_GER = "GER"
+BUS_STOP_STYLE_UK = "UK"
+BUS_STOP_STYLE_USA = "USA"
+BUS_STOP_STYLE = BUS_STOP_STYLE_SUMO
+
+BUS_STOP_AREA_COLOR = {
+    BUS_STOP_STYLE_SUMO: "#008853",
+    BUS_STOP_STYLE_USA: "#C0422C"
+}
+
+
+def set_bus_stop_style(style):
+    """
+    Sets the bus stop plotting style. Valid values are 'SUMO', 'GER', 'UK', and 'USA'.
+
+    :param style: desired bus stop plotting style
+    :return: None
+    """
+    global BUS_STOP_STYLE
+    if BUS_STOP_STYLE not in [BUS_STOP_STYLE_SUMO, BUS_STOP_STYLE_GER, BUS_STOP_STYLE_UK, BUS_STOP_STYLE_USA]:
+        raise ValueError("Invalid bus stop style '" + style + "'.")
+    BUS_STOP_STYLE = style
 
 
 class _Poly:
@@ -117,6 +144,130 @@ class _POI:
             ax.add_patch(circle)
 
 
+class _BusStop:
+    def __init__(self, attrib, reference_net):
+        """
+        Initialize a bus stop object.
+
+        :param attrib: dict of all bus stop XML attributes
+        :param reference_net: reference Sumo network object
+        :type attrib: dict
+        :type reference_net: SumoNetVis.Net
+        """
+        self.id = attrib["id"]
+        self.lane_id = attrib["lane"]
+        edge_id = "_".join(attrib["lane"].split("_")[:-1])
+        lane_num = int(attrib["lane"].split("_")[-1])
+        try:
+            self.lane = reference_net.edges.get(edge_id, None).get_lane(lane_num)
+        except (AttributeError, IndexError) as err:
+            raise IndexError("Lane " + attrib["lane"] + " does not exist in reference network.") from err
+        self.startPos = float(attrib.get("startPos", 0))
+        self.endPos = float(attrib.get("endPos", self.lane.alignment.length))
+        self.friendlyPos = attrib.get("friendlyPos", "f") in ["t", "true", "1"]
+        self.name = attrib.get("name", "")
+        self.lines = attrib.get("lines", "").split(" ")
+
+    def _get_shape(self):
+        """
+        Return the outline shape of the bus stop based on the style setting. None if shape not to be drawn.
+
+        :return: shapely geometry corresponding to bus stop outline
+        """
+        assert hasattr(ops, "substring"), "Shapely>=1.7.0 is required to plot bus stops."
+        lane_cl_seg = ops.substring(self.lane.alignment, self.startPos, self.endPos)
+        if BUS_STOP_STYLE == BUS_STOP_STYLE_SUMO:
+            outline = lane_cl_seg.parallel_offset(self.lane.width/2, "right").buffer(1, cap_style=CAP_STYLE.flat)
+        elif BUS_STOP_STYLE == BUS_STOP_STYLE_GER:
+            outline = None
+        elif BUS_STOP_STYLE == BUS_STOP_STYLE_UK:
+            outline = None
+        elif BUS_STOP_STYLE == BUS_STOP_STYLE_USA:
+            outline = lane_cl_seg.buffer(self.lane.width/2, cap_style=CAP_STYLE.flat)
+        return outline
+
+    def _get_markings(self):
+        """
+        Returns a list of lane marking objects for the bus stop based on the style setting.
+
+        :return: list of _LaneMarking objects
+        """
+        assert hasattr(ops, "substring"), "Shapely>=1.7.0 is required to plot bus stops."
+        lane_cl_seg = ops.substring(self.lane.alignment, self.startPos, self.endPos)
+        markings = []
+        if BUS_STOP_STYLE == BUS_STOP_STYLE_SUMO:
+            pass  # no markings for Sumo-style bus stops
+        elif BUS_STOP_STYLE == BUS_STOP_STYLE_GER:
+            # generate german zig-zag style markings
+            lw, dashes = 0.12, (100, 0)
+            area_width = 1.5
+            curb_align = lane_cl_seg.parallel_offset(self.lane.width/2, "right")
+            inner_align = curb_align.parallel_offset(area_width, "right")
+            n_zags = round(lane_cl_seg.length / area_width / 2)
+            if n_zags % 2 == 0:
+                n_zags += 1
+            zig_coords = [curb_align.interpolate(u).coords[0] for u in np.linspace(0, curb_align.length, n_zags)]
+            zag_coords = [inner_align.interpolate(u).coords[0] for u in np.linspace(curb_align.length, 0, n_zags)]
+            zigzag_coords = [curb_align.coords[0], inner_align.coords[-1]]
+            for i in range(n_zags):
+                if i % 2 == 0:
+                    zigzag_coords.append(tuple(zag_coords[i]))
+                else:
+                    zigzag_coords.append(tuple(zig_coords[i]))
+            zigzag_coords.append(curb_align.coords[-1])
+            zigzag_line = LineString(zigzag_coords)
+            markings.append(Net._LaneMarking(zigzag_line, lw, "w", dashes, purpose="busstop"))
+        elif BUS_STOP_STYLE == BUS_STOP_STYLE_UK:
+            # generate UK-style inset dashed line box markings
+            inset = 0.2
+            heavy_lw, light_lw = 0.3, 0.1
+            curb_dashes, end_dashes, inner_dashes = (100, 0), (1, 0.5), (0.75, 0.75)
+            curb_align = lane_cl_seg.parallel_offset(self.lane.width/2-inset-heavy_lw/2, "right")
+            inner_align = lane_cl_seg.parallel_offset(self.lane.width/2-inset-light_lw/2, "left")
+            start_edge = LineString([curb_align.coords[0], inner_align.coords[-1]])
+            end_edge = LineString([curb_align.coords[-1], inner_align.coords[0]])
+            curb_align = ops.substring(curb_align, 2*light_lw, curb_align.length-2*light_lw).\
+                parallel_offset(heavy_lw/2, "right")
+            markings.append(Net._LaneMarking(curb_align, heavy_lw, "y", curb_dashes, purpose="busstop"))
+            markings.append(Net._LaneMarking(start_edge, light_lw, "y", end_dashes, purpose="busstop"))
+            markings.append(Net._LaneMarking(end_edge, light_lw, "y", end_dashes, purpose="busstop"))
+            markings.append(Net._LaneMarking(inner_align, light_lw, "y", end_dashes, purpose="busstop"))
+        elif BUS_STOP_STYLE == BUS_STOP_STYLE_USA:
+            # return simple USA-style solid outline markings
+            lw, dashes = 0.1, (100, 0)
+            outline = lane_cl_seg.buffer(self.lane.width/2, cap_style=CAP_STYLE.flat).boundary
+            markings.append(Net._LaneMarking(outline, lw, "w", dashes, purpose="busstop"))
+        return markings
+
+    def plot(self, ax, area_kwargs=None, marking_kwargs=None, **kwargs):
+        """
+        Plot the bus stop.
+        Kwargs are passed to the plotting functions, with object-specific kwargs overriding general ones.
+
+        :param ax: matplotlib Axes object
+        :param area_kwargs: kwargs to pass to area plotting function
+        :param marking_kwargs: kwargs to pass to marking plotting function
+        :return: None
+        :type ax: plt.Axes
+        :type area_kwargs: dict
+        :type marking_kwargs: dict
+        """
+        outline = self._get_shape()
+        if area_kwargs is None:
+            area_kwargs = dict()
+        if marking_kwargs is None:
+            marking_kwargs = dict()
+        area_zorder = -80 if BUS_STOP_STYLE == BUS_STOP_STYLE_SUMO else -96
+        area_kwargs = {"zorder": area_zorder, **kwargs, **area_kwargs}
+        marking_kwargs = {"zorder": -94, **kwargs, **marking_kwargs}
+        if outline is not None:
+            area_color = BUS_STOP_AREA_COLOR.get(BUS_STOP_STYLE, "#00000000")
+            poly = matplotlib.patches.Polygon(outline.boundary.coords, True, color=area_color, **area_kwargs)
+            ax.add_patch(poly)
+        for marking in self._get_markings():
+            marking.plot(ax, **marking_kwargs)
+
+
 class Additionals:
     def __init__(self, file, reference_net=None):
         """
@@ -130,6 +281,7 @@ class Additionals:
         self.reference_net = reference_net
         self.polys = dict()
         self.pois = dict()
+        self.bus_stops = dict()
         root = ET.parse(file).getroot()
         for obj in root:
             if obj.tag == "poly":
@@ -138,6 +290,9 @@ class Additionals:
             elif obj.tag == "poi":
                 poi = _POI(obj.attrib, reference_net=reference_net)
                 self.pois[poi.id] = poi
+            elif obj.tag in ["busStop", "trainStop"]:
+                bus_stop = _BusStop(obj.attrib, reference_net=reference_net)
+                self.bus_stops[bus_stop.id] = bus_stop
 
     def plot_polygons(self, ax=None, **kwargs):
         """
@@ -167,7 +322,22 @@ class Additionals:
         for poi in self.pois.values():
             poi.plot(ax, **kwargs)
 
-    def plot(self, ax=None, polygon_kwargs=None, poi_kwargs=None, **kwargs):
+    def plot_bus_stops(self, ax=None, area_kwargs=None, marking_kwargs=None, **kwargs):
+        """
+        Plots all bus stops.
+
+        :param ax: matplotlib Axes object
+        :param area_kwargs: kwargs to pass to the bus stop area plotting function
+        :param marking_kwargs: kwargs to pass to the bus stop markings plotting function
+        :return: None
+        """
+        if ax is None:
+            ax = plt.gca()
+        for bus_stop in self.bus_stops.values():
+            bus_stop.plot(ax, area_kwargs, marking_kwargs, **kwargs)
+
+    def plot(self, ax=None, polygon_kwargs=None, poi_kwargs=None, bus_stop_area_kwargs=None,
+             bus_stop_marking_kwargs=None, **kwargs):
         """
         Plot all supported objects contained within the Additionals object.
         Kwargs are passed to the plotting functions, with object-specific kwargs overriding general ones.
@@ -175,10 +345,14 @@ class Additionals:
         :param ax: matplotlib Axes object
         :param polygon_kwargs: kwargs to pass to the polygon plotting function
         :param poi_kwargs: kwargs to pass to the POI plotting function
+        :param bus_stop_area_kwargs: kwargs to pass to the bus stop area plotting function
+        :param bus_stop_marking_kwargs: kwargs to pass to the bus stop markings plotting function
         :return: None
         :type ax: plt.Axes
         :type polygon_kwargs: dict
         :type poi_kwargs: dict
+        :type bus_stop_area_kwargs: dict
+        :type bus_stop_marking_kwargs: dict
         """
         if ax is None:
             ax = plt.gca()
@@ -190,12 +364,14 @@ class Additionals:
             poly.plot(ax, **{**kwargs, **polygon_kwargs})
         for poi in self.pois.values():
             poi.plot(ax, **{**kwargs, **poi_kwargs})
+        for bus_stop in self.bus_stops.values():
+            bus_stop.plot(ax, area_kwargs=bus_stop_area_kwargs, marking_kwargs=bus_stop_marking_kwargs, **kwargs)
 
 
 if __name__ == "__main__":
-    import Net
     net = Net.Net("../Sample/test.net.xml")
     net.plot()
+    set_bus_stop_style(BUS_STOP_STYLE_GER)
     addls = Additionals("../Sample/test.add.xml", reference_net=net)
     addls.plot()
     plt.gca().set_aspect("equal")
