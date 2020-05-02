687189efc69bd247f776c56f4295bd91461717b9
Patrick Malcolm <patrick.malcolm@tum.de>
2020-05-02 15:07:31 +0200

    add basic POI support

--- SumoNetVis/Additionals.py
+++ SumoNetVis/Additionals.py
@@ -55,6 +55,50 @@ class _Poly:
             ax.add_line(line)
 
 
+class _POI:
+    def __init__(self, attrib):
+        """
+        Initializes a Sumo additionals POI.
+
+        :param attrib: a dict of all the POI's XML attributes
+        :type attrib: dict
+        """
+        self.id = attrib["id"]
+        self.color = _Utils.convert_sumo_color(attrib["color"])
+        self.x, self.y = None, None
+        if "x" in attrib and "y" in attrib:
+            self.x = float(attrib["x"])
+            self.y = float(attrib["y"])
+        elif "lane" in attrib and "lanePos" in attrib:
+            warnings.warn("POI locations defined relative to lanes not supported.")
+        elif "lat" in attrib and "lon" in attrib:
+            warnings.warn("POI locations defined as lat/lon not supported.")
+        else:
+            raise ValueError("POI " + self.id + " has no valid position attributes.")
+        self.type = attrib.get("type", "")
+        self.layer = float(attrib.get("layer", "0"))
+        self.imgFile = attrib.get("imgFile", None)
+        if self.imgFile is not None:
+            warnings.warn("Display of POIs in additional files as images not supported.")
+        self.width = float(attrib.get("width", 0))
+        self.height = float(attrib.get("height", 0))
+        self.angle = float(attrib.get("angle", 0))
+
+    def plot(self, ax, **kwargs):
+        """
+        Plot the POI.
+
+        :param ax: matplotlib Axes object
+        :param kwargs: kwargs to pass to the plotting function
+        :return: None
+        :type ax: plt.Axes
+        """
+        kwargs = {"radius": 1, "zorder": self.layer-110 if self.layer <= 0 else self.layer-90, **kwargs}
+        if self.x is not None and self.y is not None:
+            circle = matplotlib.patches.Circle((self.x, self.y), color=self.color, **kwargs)
+            ax.add_patch(circle)
+
+
 class Additionals:
     def __init__(self, file):
         """
@@ -63,11 +107,15 @@ class Additionals:
         :param file: path to Sumo additional file
         """
         self.polys = dict()
+        self.pois = dict()
         root = ET.parse(file).getroot()
         for obj in root:
             if obj.tag == "poly":
                 poly = _Poly(obj.attrib)
                 self.polys[poly.id] = poly
+            elif obj.tag == "poi":
+                poi = _POI(obj.attrib)
+                self.pois[poi.id] = poi
 
     def plot_polygons(self, ax=None, **kwargs):
         if ax is None:
@@ -75,9 +123,16 @@ class Additionals:
         for poly in self.polys.values():
             poly.plot(ax, **kwargs)
 
+    def plot_pois(self, ax=None, **kwargs):
+        if ax is None:
+            ax = plt.gca()
+        for poi in self.pois.values():
+            poi.plot(ax, **kwargs)
+
 
 if __name__ == "__main__":
     addls = Additionals("../Sample/test.add.xml")
     addls.plot_polygons()
+    addls.plot_pois()
     plt.gca().set_aspect("equal")
     plt.show()
