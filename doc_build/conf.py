e61b448872d504eeef2fba33b20f29e3a70c0604
patmalcolm91 <patrick.malcolm@tum.de>
2024-11-10 18:28:10 +0100

    version bump to 1.6.1

--- doc_build/conf.py
+++ doc_build/conf.py
@@ -26,7 +26,7 @@ copyright = '2020, Patrick Malcolm'
 author = 'Patrick Malcolm'
 
 # The full version, including alpha/beta/rc tags
-release = '1.6.0'
+release = '1.6.1'
 
 
 # -- General configuration ---------------------------------------------------
